8e8426db93593b035f062ec975d20bc183ea9690
Yannick <huyiyan1986@gmail.com>
2018-12-06 21:23:42 +0100

    initialize data from mock

---
+++ src/components/BookList.vue
@@ -0,0 +1,22 @@
+<template>
+    <ol>
+        <li v-for="(item, index) in questions" :key="item.id">
+            <h4>{{item.name}} - <span class="book-title">{{item.book}}</span></h4>
+        </li>
+    </ol>
+</template>
+
+<script>
+    
+    export default {
+        name: "BookList",
+        props: ["questions"]
+    }
+</script>
+
+<style lang="css">
+    .book-title{
+        font-style: italic;
+    }
+</style>
+
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+   
+    <BookList :questions="Questions" />
+  </div>
+</template>
+
+<script>
+import BookList from './BookList.vue';
+import Questions from '../assets/questions-data.json';
+
+export default {
+  name: 'Dashboard',
+  props: {
+    msg: String
+  },
+  data(){
+    return {
+      Questions
+    }
+  },
+  components: {
+    BookList
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
